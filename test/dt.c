2fa5b6aeb9666415eda6077a54e7b1c67f610faf
Phillip Johnston <phillip@embeddedartistry.com>
2022-03-11 07:59:27 -0800

    WIP: expand dt.c test support

--- test/dt.c
+++ test/dt.c
@@ -12,6 +12,7 @@
 #include "ulpsDistance.h"
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #ifdef __cplusplus
 extern "C" char* dtoa(double, int, int, int*, int*, char**);
@@ -21,138 +22,51 @@ extern char* dtoa(double, int, int, int*, int*, char**);
 
 #define length(x) (sizeof(x) / sizeof *(x))
 
+#define DEFAULT_DTOA_MODE 0
+#define DEFAULT_DTOA_NUM_DIGITS 17
+
 static struct
 {
 	const char* s;
+	int dtoa_mode;
+	int dtoa_digits;
 	double f;
+	int sign;
+	int dcept;
+	int num_digits;
+	const char* digits;
+	int expected_errno;
 } test_values[] = {
-	{"1.23", 1.23},
-	{"1.23e+20", 123000000000000000000.0},
-	{"1.23e-20", 1.23e-20},
-	{"1.23456789", 1.23456789},
-	{"1.23456589e+20", 1.23456589e+20},
-	{"1.23e+30", 1.23e+30},
-	{"1.23e-30", 1.23e-30},
-	{"1.23456789e-20", 1.23456789e-20},
-	{"1.23456789e-30", 1.23456789e-30},
-	{"1.234567890123456789", 1.234567890123456789},
-	{"1.23456789012345678901234567890123456789", 1.23456789012345678901234567890123456789},
-	{"1.23e306", 1.23e306},
-	{"1.23e-306", 1.23e-306},
-	{"1.23e-320", 1.23e-320},
-	{"1.23e-20", 1.23e-20},
-	{"1.23456789e307", 1.23456789e307},
-	{"1.23456589e-307", 1.23456589e-307},
-	{"1.234567890123456789", 1.234567890123456789},
-	{"1.234567890123456789e301", 1.234567890123456789e301},
-	{"1.234567890123456789e-301", 1.234567890123456789e-301},
-	{"1.234567890123456789e-321", 1.234567890123456789e-321},
-	{"1e23", 1e23},
-	{"1e310", __builtin_inf()},
-	{"9.0259718793241475e-277", 9.0259718793241475e-277},
-	{"9.025971879324147880346310405869e-277", 9.025971879324147880346310405869e-277},
-	{"9.025971879324147880346310405868e-277", 9.025971879324147880346310405868e-277},
-	{"2.2250738585072014e-308", 2.2250738585072014e-308},
-	{"2.2250738585072013e-308", 2.2250738585072013e-308},
-};
-
-#if 0
-Sample output
-[10:53:16] gdtoa {master} $ cat test/testnos | ./buildresults/test/gdtoa_dt
-Input: 1.23
-Output: d =
-1.23 = 0x3ff3ae14 7ae147ae, se =
-	g_fmt gives "1.23"
-	dtoa(mode = 0, ndigits = 17):
-	dtoa returns sign = 0, decpt = 1, 3 digits:
-123
-	nextafter(d,+Inf) = 1.2300000000000002 = 0x3ff3ae14 7ae147af:
-zsh: done                cat test/testnos |
-
-0 = 0x0 0, se = (lldb) target create "cat"
-	g_fmt gives "0"
-	dtoa(mode = 0, ndigits = 17):
-	dtoa returns sign = 0, decpt = 1, 1 digits:
-0
-	nextafter(d,+Inf) = 4.9406564584124654e-324 = 0x0 1:
-	g_fmt gives "0"
-	dtoa returns sign = 0, decpt = 1, 1 digits:
-0
-sent d = 4.9406564584124654e-324 = 0x0 1, buf = .0e1
-got d1 = 0 = 0x0 0
-Input: Current executable set to 'cat' (x86_64).
-Output: d =
-0 = 0x0 0, se = Current executable set to 'cat' (x86_64).
-	g_fmt gives "0"
-	dtoa(mode = 0, ndigits = 17):
-	dtoa returns sign = 0, decpt = 1, 1 digits:
-0
-	nextafter(d,+Inf) = 4.9406564584124654e-324 = 0x0 1:
-	g_fmt gives "0"
-	dtoa returns sign = 0, decpt = 1, 1 digits:
-0
-sent d = 4.9406564584124654e-324 = 0x0 1, buf = .0e1
-got d1 = 0 = 0x0 0
-Input: (lldb) settings set -- target.run-args  "test/testnos1"
-Output: d =
-0 = 0x0 0, se = (lldb) settings set -- target.run-args  "test/testnos1"
-	g_fmt gives "0"
-	dtoa(mode = 0, ndigits = 17):
-	dtoa returns sign = 0, decpt = 1, 1 digits:
-0
-	nextafter(d,+Inf) = 4.9406564584124654e-324 = 0x0 1:
-	g_fmt gives "0"
-	dtoa returns sign = 0, decpt = 1, 1 digits:
-0
-#endif
-
-// Defaults: int mode = 0, ndigits = 17;
-static void dt_test1_atof(void** state)
-{
-	(void)state;
-	char *s = NULL, *se = NULL;
-	int decpt, sign;
-
-	errno = 0;
-
-	for(size_t i = 0; i < length(test_values); i++)
-	{
-		double atof_conv = atof(test_values[i].s);
-		printf("i: %zu, atof: %1.30f, expected: %1.30f, distance: %lld, errno: %d (%s)\n", i, atof_conv, test_values[i].f, ulpsDistanceDouble(atof_conv, test_values[i].f), errno, errno == 0? "OK" : strerror(errno));
-		assert_false(errno);
-#if 0
-		s = dtoa(atof_conv, 0, 17, &decpt, &sign, &se);
-		printf("\tdtoa(mode = %d, ndigits = %d):\n", 0, 17);
-		printf("\tdtoa returns sign = %d, decpt = %d, %ld digits: %s\n", sign, decpt, se - s, s);
-#endif
-		assert_true(ulpsDistanceDouble(atof_conv, test_values[i].f) < 2);
-		if(s)
-		{
-			freedtoa(s);
-		}
-	}
-
-}
-
-static void dt_test1_strtod(void** state)
-{
-	(void)state;
-	char* unused;
-
-	errno = 0;
-
-	for(size_t i = 0; i < length(test_values); i++)
-	{
-		double strtod_conv = strtod(test_values[i].s, &unused);
-		int64_t conv_int, expected_int;
-		memcpy(&conv_int, &strtod_conv, sizeof(double));
-		memcpy(&expected_int, &test_values[i].f, sizeof(double));
-		printf("i: %zu, strtod: 0x%x, expected: 0x%x, distance: %lld, errno: %d (%s)\n", i, conv_int, expected_int, ulpsDistanceDouble(strtod_conv, test_values[i].f), errno, errno == 0? "OK" : strerror(errno));
-		assert_false(errno);
-		assert_true(ulpsDistanceDouble(strtod_conv, test_values[i].f) < 2);
-	}
-
-}
+	// Default Mode:NumDigits tests (0:17)
+	{"1.23", DEFAULT_DTOA_MODE, DEFAULT_DTOA_NUM_DIGITS, 1.23, 0, 1, 3, "123", 0},
+	{"1.23e+20", DEFAULT_DTOA_MODE, DEFAULT_DTOA_NUM_DIGITS, 123000000000000000000.0, 0, 21, 3, "123", 0},
+	{"1.23e-20", DEFAULT_DTOA_MODE, DEFAULT_DTOA_NUM_DIGITS, 1.23e-20, 0, -19, 3, "123", 0},
+	{"1.23456789", DEFAULT_DTOA_MODE, DEFAULT_DTOA_NUM_DIGITS, 1.23456789, 0, 1, 9, "123456789", 0},
+	{"1.23456589e+20", DEFAULT_DTOA_MODE, DEFAULT_DTOA_NUM_DIGITS, 1.23456589e+20, 0, 21, 9, "123456589", 0},
+	{"1.23e+30", DEFAULT_DTOA_MODE, DEFAULT_DTOA_NUM_DIGITS, 1.23e+30, 0, 31, 3, "123", 0},
+	{"1.23e-30", DEFAULT_DTOA_MODE, DEFAULT_DTOA_NUM_DIGITS, 1.23e-30, 0, -29, 3, "123", 0},
+	{"1.23456789e-20", DEFAULT_DTOA_MODE, DEFAULT_DTOA_NUM_DIGITS, 1.23456789e-20, 0, -19, 9, "123456789", 0},
+	{"1.23456789e-30", DEFAULT_DTOA_MODE, DEFAULT_DTOA_NUM_DIGITS, 1.23456789e-30, 0, -29, 9, "123456789", 0},
+	{"1.234567890123456789", DEFAULT_DTOA_MODE, DEFAULT_DTOA_NUM_DIGITS, 1.234567890123456789, 0, 1, 17, "12345678901234567", 0},
+	{"1.23456789012345678901234567890123456789", DEFAULT_DTOA_MODE, DEFAULT_DTOA_NUM_DIGITS, 1.23456789012345678901234567890123456789, 0, 1, 17, "12345678901234567", 0},
+	{"1.23e306", DEFAULT_DTOA_MODE, DEFAULT_DTOA_NUM_DIGITS, 1.23e306, 0, 307, 3, "123", 0},
+	{"1.23e-306", DEFAULT_DTOA_MODE, DEFAULT_DTOA_NUM_DIGITS, 1.23e-306, 0, -305, 3, "123", 0},
+	{"1.23e-320", DEFAULT_DTOA_MODE, DEFAULT_DTOA_NUM_DIGITS, 1.23e-320, 0, -319, 3, "123", 0},
+	{"1.23e-20", DEFAULT_DTOA_MODE, DEFAULT_DTOA_NUM_DIGITS, 1.23e-20, 0, -19, 3, "123", 0},
+	{"1.23456789e307", DEFAULT_DTOA_MODE, DEFAULT_DTOA_NUM_DIGITS, 1.23456789e307, 0, 308, 9, "123456789", 0},
+	{"1.23456589e-307", DEFAULT_DTOA_MODE, DEFAULT_DTOA_NUM_DIGITS, 1.23456589e-307, 0, -306, 9, "123456789", 0},
+	{"1.234567890123456789", DEFAULT_DTOA_MODE, DEFAULT_DTOA_NUM_DIGITS, 1.234567890123456789, 0, 1, 17, "12345678901234567", 0},
+	{"1.234567890123456789e301", DEFAULT_DTOA_MODE, DEFAULT_DTOA_NUM_DIGITS, 1.234567890123456789e301, 0, 302, 17, "12345678901234568", 0},
+	{"1.234567890123456789e-301", DEFAULT_DTOA_MODE, DEFAULT_DTOA_NUM_DIGITS, 1.234567890123456789e-301, 0, -300, 17, "12345678901234568", 0},
+	{"1.234567890123456789e-321", DEFAULT_DTOA_MODE, DEFAULT_DTOA_NUM_DIGITS, 1.234567890123456789e-321, 0, -320, 17, "12345678901234568", 0},
+	{"1e23", DEFAULT_DTOA_MODE, DEFAULT_DTOA_NUM_DIGITS, 1e23, 0, 24, 1, "1", 0},
+	{"1e310", DEFAULT_DTOA_MODE, DEFAULT_DTOA_NUM_DIGITS, __builtin_inf(), 0, 9999, 8, "Infinity", ERANGE},
+	{"9.0259718793241475e-277", DEFAULT_DTOA_MODE, DEFAULT_DTOA_NUM_DIGITS, 9.0259718793241475e-277, 0, -276, 16, "9025971879324148", 0},
+	{"9.025971879324147880346310405869e-277", DEFAULT_DTOA_MODE, DEFAULT_DTOA_NUM_DIGITS, 9.025971879324147880346310405869e-277, 0, -276, 16, "9025971879324148", 0},
+	{"9.025971879324147880346310405868e-277", DEFAULT_DTOA_MODE, DEFAULT_DTOA_NUM_DIGITS, 9.025971879324147880346310405868e-277, 0, -276, 16, "9025971879324148", 0},
+	{"2.2250738585072014e-308", DEFAULT_DTOA_MODE, DEFAULT_DTOA_NUM_DIGITS, 2.2250738585072014e-308, 0, -307, 17, "22250738585072014", 0},
+	{"2.2250738585072013e-308", DEFAULT_DTOA_MODE, DEFAULT_DTOA_NUM_DIGITS, 2.2250738585072013e-308, 0, -307, 17, "22250738585072014", 0},
+	// Adjusted Mode:NumDigits tests
 
 #if 0
 Test 2 inputs
@@ -178,12 +92,45 @@ Test 2 inputs
 1.234565e-20:2 6
 1.234565e-20:4 6
 #endif
+};
+
+static void dt_test(void** state)
+{
+	(void)state;
+	char* unused;
+	int dcept = 0, sign = 0;
+	char *s = NULL, *se = NULL;
+
+	for(size_t i = 0; i < length(test_values); i++)
+	{
+		errno = 0;
+		printf("Running entry %zu\n", i);
+		double strtod_conv = strtod(test_values[i].s, &unused);
+		assert_true(errno == test_values[i].expected_errno);
+
+		s = dtoa(strtod_conv, test_values[i].dtoa_mode, test_values[i].dtoa_digits, &dcept, &sign, &se);
+		int64_t conv_int, expected_int;
+		memcpy(&conv_int, &strtod_conv, sizeof(double));
+		memcpy(&expected_int, &test_values[i].f, sizeof(double));
+		printf("sign: %d, dcept: %d, num_digits: %ld, digits: %s\n", sign, dcept, se - s, s);
+		printf("EXPECTED sign: %d, dcept: %d, num_digits: %d, digits: %s\n", test_values[i].sign, test_values[i].dcept, test_values[i].num_digits, test_values[i].digits);
+		printf("i: %zu, strtod: 0x%llx, expected: 0x%llx, distance: %lld, errno: %d (%s)\n", i, conv_int, expected_int, ulpsDistanceDouble(strtod_conv, test_values[i].f), errno, errno == 0? "OK" : strerror(errno));
+
+		assert_true(sign == test_values[i].sign);
+		assert_true(dcept == test_values[i].dcept);
+		assert_true((se - s) == test_values[i].num_digits);
+		assert_true(0 == strcmp(s, test_values[i].digits));
+		assert_true(ulpsDistanceDouble(strtod_conv, test_values[i].f) < 2);
+	}
+
+}
+
+
 
 int dt_tests()
 {
 	const struct CMUnitTest dt_tests[] = {
-	//	cmocka_unit_test(dt_test1_atof),
-		cmocka_unit_test(dt_test1_strtod),
+		cmocka_unit_test(dt_test),
 	};
 
 	return cmocka_run_group_tests(dt_tests, NULL, NULL);
